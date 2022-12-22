b392fe6d1eacca14c68f003718a1a30771a3a00f
Frank Grullon <frankgrullon@Franks-MacBook-Pro.local>
2022-12-21 20:24:29 -0400

    edit packages

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
+mongoose.set('strictQuery', false);
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
